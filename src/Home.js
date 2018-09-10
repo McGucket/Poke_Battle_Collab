93b4b8619a83403cb89d91a2c3fffcb3d7061064
SamanthaChia <samantha1999@live.com.sg>
2018-09-10 20:01:27 +0800

    Logo

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,16 @@
+var logo = require('./PicturesUsed/PokemonLogo.png');
 var React = require('react');
 var Link = require('react-router-dom').Link;
+
 
 class Home extends React.Component {
     render() {
         return(
+            <div className="This">
+            <img src={logo} alt="Logo"/>
+            <button><img src={require('./PicturesUsed/ClosePokeball.png')} alt="pokeball" onClick={this.myfunction} /></button>
+            </div>
             
-            <Link className='button' to='/instructions'>
-           
-            </Link>
         )
     }
 }
