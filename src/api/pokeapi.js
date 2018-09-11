d48eebed166155029c9929d10f0bf761b37594d9
Owen Lee <owenleeweihern@gmail.com>
2018-09-11 19:00:59 +0800

    "Finished pokedex data

--- src/api/pokeapi.js
+++ src/api/pokeapi.js
@@ -1,21 +1,25 @@
 var axios = require('axios');
 var PropTypes = require('prop-types');
-var jsonURL = window.encodeURI("https://api.myjson.com/bins/1eln1c"); //Not really needed, link is already a URL
+var jsonURL = "http://localhost:3000/Pokemon"; //Not really needed, link is already a URL
 
 module.exports = {
     fetchAllPokemons: function () {
 
         return axios.get(jsonURL)
             .then(response => {
-                console.log("Poke-List", response.data.Pokemon);
-                return response.data.Pokemon;
+                console.log(response.data);
+                return response.data;
             });
     },
-    getPokedexData: function (pokeName) {
-        return axios.get(jsonURL)
+    getPokedexData: function () {
+        var selectedPokemon = sessionStorage.getItem('pokeId')
+        var searchURL = window.encodeURI(jsonURL + "/" + selectedPokemon)
+
+        return axios.get(searchURL)
             .then(response => {
-                var pokeArr = response.data.Pokemon;
-                console.log("Pokemon-List", pokeArr[3]);
-            });
+                var resp = response.data;
+                return console.log(resp);
+            })
     }
 }
+
