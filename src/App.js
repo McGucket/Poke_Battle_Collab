b430bde07a1c134473553c51476080c907753fa3
SamanthaChia <samantha1999@live.com.sg>
2018-09-11 10:52:18 +0800

    finished HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 var Home = require('./Home');
+var Instructions = require('./Instructions');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Nav = require('./Nav');
+var Switch = ReactRouter.Switch;
 
 class App extends Component {
   render() {
@@ -11,7 +13,13 @@ class App extends Component {
        <Router>
 <div className="App">
       <Nav />
-      <Route path='/' component={Home}/>
+      <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/instruction' component={Instructions}/>
+      <Route render={function() {
+                    return <p>Not Found</p>
+                }} />
+      </Switch>
       </div>
        </Router>
       
