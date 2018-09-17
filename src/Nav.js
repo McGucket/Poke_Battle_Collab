21c95aca1b8e4fcdd06c528dfce95024e8cfae2d
SamanthaChia <samantha1999@live.com.sg>
2018-09-17 15:49:01 +0800

    linting test

--- src/Nav.js
+++ src/Nav.js
@@ -1,30 +1,30 @@
-var React = require('react');
-var NavLink = require('react-router-dom').NavLink;
-var battleMusic = require('./battleMusic');
+const React = require('react');
+const NavLink = require('react-router-dom').NavLink;
+const battleMusic = require('./battleMusic');
 
-function Nav () {
-    return(
-        <ul className='nav'>
-            <li>
-                <NavLink exact activeClassName='active' to='/' onClick={() => battleMusic.stopMusic()}>
+function Nav() {
+  return (
+    <ul className="nav">
+      <li>
+        <NavLink exact activeClassName="active" to="/" onClick={() => battleMusic.stopMusic()}>
                 Home
-                </NavLink>
-            </li>
+        </NavLink>
+      </li>
 
-            <li>
-                <NavLink activeClassName='active' to='/instructions' onClick={() => battleMusic.stopMusic()}>
+      <li>
+        <NavLink activeClassName="active" to="/instructions" onClick={() => battleMusic.stopMusic()}>
                 Instructions
-                </NavLink>
-            </li>
+        </NavLink>
+      </li>
 
-            <li>
-                <NavLink activeClassName='active' to='/pokedex' onClick={() => battleMusic.stopMusic()}>
+      <li>
+        <NavLink activeClassName="active" to="/pokedex" onClick={() => battleMusic.stopMusic()}>
                 Pokedex
-                </NavLink>
-            </li>
-            
-        </ul>
-    )
+        </NavLink>
+      </li>
+
+    </ul>
+  );
 }
 
 module.exports = Nav;
--- src/SelectPokemon.js
+++ src/SelectPokemon.js
@@ -1,109 +1,105 @@
-var header = require('./PicturesUsed/PokeBattleTitle.png');
-var React = require('react');
-var pokeApi = require('./api/pokeapi');
-var Link = require('react-router-dom').Link;
-var PropTypes = require('prop-types');
+const React = require('react');
+const Link = require('react-router-dom').Link;
+const PropTypes = require('prop-types');
+const pokeApi = require('./api/pokeapi');
+const header = require('./PicturesUsed/PokeBattleTitle.png');
 
 class GetCharacters extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            battleArr: []
-        }
+    this.state = {
+      battleArr: [],
+    };
 
-        this.handleOnClick = this.handleOnClick.bind(this);
-    }
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-    handleOnClick(event) {
-        let id = event.target.id
-        let battleArray = this.state.battleArr.concat([id])
-        battleArray.splice(2,1)
-        this.setState(() => {
-            return{
-            battleArr: battleArray
-            
+  handleOnClick(event) {
+    const id = event.target.id;
+    const battleArray = this.state.battleArr.concat([id]);
+    battleArray.splice(2, 1);
+    this.setState(() => ({
+      battleArr: battleArray,
 
-        }}, () => console.log(this.state.battleArr))
 
-    }
+    }), () => console.log(this.state.battleArr));
+  }
 
-    render() {
-        return (
-            <div>
-                {this.state.battleArr.length === 0 ?
-                    <h2 className='selectPokemonHeader'>Choose Your Pokemon!</h2>
-                    :
-                    <h2 className='selectPokemonHeader2'>Choose Enemy Pokemon! (Press the Pokeball when done!)</h2>
+  render() {
+    return (
+      <div>
+        {this.state.battleArr.length === 0
+          ? <h2 className="selectPokemonHeader">Choose Your Pokemon!</h2>
+          : <h2 className="selectPokemonHeader2">Choose Enemy Pokemon! (Press the Pokeball when done!)</h2>
                 }
-                <div className='pokedexBack'>
-                    <ul className="dexList">
-                        {this.props.pokemons.map((pokemon) => {
-                            return (
-                                <li key={pokemon.id}>
-                                    <img className='select_pokemon' id={pokemon.id} onClick={this.handleOnClick.bind(this)} src={pokemon.imgSrcFront} alt={pokemon.pokemonName} width='200' />
-                                </li>
+        <div className="pokedexBack">
+          <ul className="dexList">
+            {this.props.pokemons.map(pokemon => (
+              <li key={pokemon.id}>
+                <img className="select_pokemon" id={pokemon.id} onClick={this.handleOnClick.bind(this)} src={pokemon.imgSrcFront} alt={pokemon.pokemonName} width="200" />
+              </li>
 
-                            )
-                        })}
+            ))}
 
 
-                    </ul>
+          </ul>
+        </div>
+        {this.state.battleArr.length >= 2
+                && (
+                <div className="battleBTN">
+                  <Link
+                    className="battleLink"
+                    to={{
+                      pathname: '/Battle',
+                      search: `?HeroPokemon=${this.state.battleArr[0]}&EnemyPokemon=${this.state.battleArr[1]}`,
+                    }}
+                  >
+Battle
+                  </Link>
                 </div>
-                {this.state.battleArr.length >= 2 &&
-                <div className='battleBTN'>
-                    <Link className='battleLink' to={{
-                        pathname: '/Battle',
-                        search: '?HeroPokemon=' + this.state.battleArr[0] + '&EnemyPokemon=' + this.state.battleArr[1]
-                    }}>Battle</Link>
-                </div>}
-            </div>
-        );
-    }
+                )}
+      </div>
+    );
+  }
 }
 
 GetCharacters.propTypes = {
-    id: PropTypes.number,
-    imgSrcFront: PropTypes.string,
-    pokemonName: PropTypes.string
-}
-
+  id: PropTypes.number,
+  imgSrcFront: PropTypes.string,
+  pokemonName: PropTypes.string,
+};
 
 
 class SelectPokemon extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pokemons: [],
-            selectPokemons: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [],
+      selectPokemons: [],
+    };
+  }
 
 
+  componentDidMount() {
+    pokeApi.fetchAllPokemons()
+      .then((pokemons) => {
+        this.setState(() => ({
+          pokemons,
+        }));
+      });
+  }
 
 
-    componentDidMount() {
-        pokeApi.fetchAllPokemons()
-            .then(function (pokemons) {
-                this.setState(function () {
-                    return {
-                        pokemons
-                    }
-                });
-            }.bind(this))
+  render() {
+    return (
+      <div>
+        <img className="pokemonbattle_header" src={header} alt="title" />
+        <GetCharacters pokemons={this.state.pokemons} />
 
-    }
-
-
-    render() {
-        return (
-            <div>
-                <img className="pokemonbattle_header" src={header} alt="title" />
-                <GetCharacters pokemons={this.state.pokemons} />
-
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 module.exports = SelectPokemon;
